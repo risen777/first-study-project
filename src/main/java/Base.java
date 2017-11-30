57fc501d2a9ebb030c05f0030236eb98c0c1d595
risen777 <21risen@gmail.com>
2017-11-30 22:57:30 +0300

    I wrote a Calculator

---
+++ src/main/java/Base.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+/**Created by Sergei on 29.11.17
+ * @author Sergei
+ * @see #someMethod()
+  */
+public class Base {
+
+    /*First*/
+    public static void main(String[] args) {
+        Scanner scanner= new Scanner(System.in);
+        System.out.println("enter int");
+        int i =scanner.nextInt();
+        System.out.println("enter str");
+        String text=scanner.next();
+        System.out.println("Integer"+ i);
+        System.out.println("String"+ text);
+        System.err.println("This is error stream");
+        scanner.close();
+
+    }
+public String someMethod(){
+return "string";
+
+}
+
+
+
+}
