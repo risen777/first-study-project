434c9fdc38e66d2231001dd348662de256100017
risen777 <21risen@gmail.com>
2018-01-08 07:07:34 +0300

    Added metod Write

--- src/main/java/lesson/fourth/Application.java
+++ src/main/java/lesson/fourth/Application.java
@@ -13,7 +13,7 @@ import java.util.Scanner;
 
 public class Application {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
         Gift gift = new Gift();
 
